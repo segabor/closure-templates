419efe24b7c491d603e5c12ca6986b43f6cbc56b
Gábor Sebestyén <segabor@gmail.com>
2017-12-29 07:14:26 +0100

    Fixed joining String arrays

--- java/src/com/google/template/soy/swiftsrc/restricted/SwiftListExpr.java
+++ java/src/com/google/template/soy/swiftsrc/restricted/SwiftListExpr.java
@@ -9,7 +9,6 @@ public final class SwiftListExpr extends SwiftExpr {
   @Override
   public SwiftStringExpr toSwiftString() {
     // Lists are converted by concatenating all of their values.
-	// FIXME
-    return new SwiftStringExpr("''.join(" + getText() + ")", Integer.MAX_VALUE);
+    return new SwiftStringExpr("" + getText() + ".joined()", Integer.MAX_VALUE);
   }
 }
